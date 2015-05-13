7b111cc89d72034dfae43cc0a7b7068f9a0897d9
Shae Judge <shaejudge@gmail.com>
2015-05-12 23:49:15 -0400

    Actually completed assignment

--- src/videoStore.php
+++ src/videoStore.php
@@ -24,7 +24,7 @@
 	<br>
 	
 <?php 
-		var_dump($_POST);
+		//var_dump($_POST);
 		//die;	
 	
 		//ini_set('display_errors', 'On');
@@ -36,11 +36,12 @@
 		if ($mysqli->connect_errno) {
 			echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 		}else{
-			echo "Connection Worked!<br>";
+			//echo "Connection Worked!<br>";
 		}
 
 	if(isset($_POST['Add'])){
-	
+		$mysqli = new mysqli("oniddb.cws.oregonstate.edu", "judges-db", "6rmoErrYBdGgdSmP", "judges-db");
+		
 		$sql = "INSERT INTO Video_Store(name, category, length) VALUES (?, ?, ?)";
 		$stmt = $mysqli->prepare($sql);
 		$stmt->bind_param("ssi", $name, $category, $length);
@@ -50,6 +51,7 @@
 		$length = $_POST['videoLen'];
 		$stmt->execute();
 		
+		/*
 		if (!($stmt = $mysqli->prepare($sql))) {
 			echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
 		}else{
@@ -67,6 +69,7 @@
 		}else{
 			echo "Executed!";
 		}
+		*/
 				
 		$stmt->close();
 		$mysqli->close();	
@@ -74,6 +77,23 @@
 	
 	
 ?>
+	
+	<select id="cd" name="cd">
+        
+        <?php
+            $mysqli = new mysqli("oniddb.cws.oregonstate.edu", "judges-db", "6rmoErrYBdGgdSmP", "judges-db");
+            
+			$query = "SELECT category FROM Video_Store";
+            $result = mysql_query($query);
+            
+            while ($cdrow = mysql_fetch_array($result)) {
+            $droplist = $droplistshow["categories"];
+                echo "<option>" . $categories . "</option>";
+            }
+                
+        ?>
+    
+        </select>
 	
 	<h3>Video Database</h3>
 	<table style="width:50%" border="1">
@@ -84,65 +104,97 @@
 			<th>Availability</th>
 			<th><label for="ch-in">Check-in</label></th>
 			<th><label for="ch-out">Check-out</label></th>
+			<th>Check In/Out</th>
 			<th>Delete</th>
 		</tr>
 		<tr>
 		<?php
-			
-			$sql = "SELECT id, name, category, length, rented FROM Video_Store";
-			$result = $mysqli->query($sql);
+			$mysqli = new mysqli("oniddb.cws.oregonstate.edu", "judges-db", "6rmoErrYBdGgdSmP", "judges-db");
+			$query = "SELECT id, name, category, length, rented FROM Video_Store";
+			$result = $mysqli->query($query);
+				
 				
 				if(isset($_POST['Delete'])){
-					$db_name = $_POST['db_name'];
-					$sql = "DELETE FROM `Video_Store` WHERE `name`='$db_name'";
+					$mysqli = new mysqli("oniddb.cws.oregonstate.edu", "judges-db", "6rmoErrYBdGgdSmP", "judges-db");
+					
+					$stmt = $mysqli->prepare("DELETE FROM Video_Store WHERE name = ?");
+					//var_dump($dbName);
+					$stmt->bind_param('s', $_POST['dbName']);
+					$stmt->execute(); 
+					$stmt->close();
 				}
 				
 				if(isset($_POST['deleteAll'])){
-					mysql_query("DELETE FROM Video_Store");
+					$mysqli = new mysqli("oniddb.cws.oregonstate.edu", "judges-db", "6rmoErrYBdGgdSmP", "judges-db");
+					
+					$stmt = $mysqli->prepare("DELETE FROM Video_Store");
+					$stmt->bind_param();
+					$stmt->execute(); 
+					$stmt->close();
 				}
 				
 				
 				
 				if(isset($_POST['subCheck'])){
-					$db_name = $_POST['db_name'];
-					$check = $_POST['check'];
+					$mysqli = new mysqli("oniddb.cws.oregonstate.edu", "judges-db", "6rmoErrYBdGgdSmP", "judges-db");
 					
-					if($check == "Check-in"){
-					$sql = "UPDATE Video_Store SET rented=0 WHERE name='$db_name'";
+					$dbName = $_POST['dbName'];
+					
+					
+					if($_POST['check'] == "checkIn"){
+						$stmt = $mysqli->prepare("UPDATE Video_Store SET rented = ? WHERE name = ?");
+						$zero = 0;
+						$stmt->bind_param('is', $zero, $dbName);
+						$stmt->execute(); 
+						$stmt->close();
 					}
 					
-					if($check == "Check-out"){
-					$sql = "UPDATE Video_Store SET rented=1 WHERE name='$db_name'";
+					if($_POST['check'] == "checkOut"){
+						$stmt = $mysqli->prepare("UPDATE Video_Store SET rented = ? WHERE name = ?");
+						$one = 1;
+						$stmt->bind_param('is', $one, $dbName);
+						$stmt->execute(); 
+						$stmt->close();
 					}
 				}
 				
-				if($row["rented"] == 0){
-					$available = "Available!";
-				}else{
-					$available = "Checked out";
-				}
+
 				
-				while($row = $result->fetch_assoc()) {
+				while($row = $result->fetch_array()) {
+					
+					if($row["rented"] == 0){
+						$available = "Available!";
+						$checked = "checked";
+					}else{
+						$available = "Checked out";
+						$checked = "checked";
+					}
+					
 					echo '<form action="videoStore.php" method="POST">';
 					echo '<td style="display:none;"><input type="text" name=db_id value="' . $row["id"]. '"></td>';
-					echo '<td><input type="text" name=db_name value="' . $row["name"]. '" readonly></td>';
+					echo '<td><input type="text" name=dbName value="' . $row["name"]. '" readonly></td>';
 					echo "<td>" . $row["category"]. "</td>";
 					echo "<td>" . $row["length"]. "</td>";
 					echo "<td align='center'>" . $available . "</td>";
-					echo '<td align="center"><input type="radio" name="check" id="ch-in" value="Check-in" /></td>';
-					echo '<td align="center"><input type="radio" name="check" id="ch-out" value="Check-out" /></td>';
+					echo '<td align="center"><input type="radio" name="check" id="ch-in" value="checkIn"/></td>';
+					echo '<td align="center"><input type="radio" name="check" id="ch-out" value="checkOut"/></td>';
+					echo '<td align="center"><input type="submit" value="Submit" name="subCheck"></td>';
 					echo '<td align="center"><input type="submit" value="Delete" name="Delete"></td>';
 					echo "</form>";
 					echo '</tr>';
 				}	
 		?>
-					<td colspan="7"  align="center"><input type="submit" value="Submit Check-in/out" name="subCheck">
+					
 					</form>
-					</td>
+					
 	</table>
 					<br>
-					<form action="videoStore.php" method="POST">
+					<form action="" method="POST">
 					<input type="submit" value="Delete All" name="deleteAll">
+					</form>
+		
+					<form action="" method="POST">
+					<input type="submit" value="Save All Changes" name="Save">
 					</form>
 					<br>
 					
